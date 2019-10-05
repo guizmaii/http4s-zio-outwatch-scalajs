9285cb68616912c324b55a24e4bbc6bb214932b6
jules Ivanic <jules.ivanic@gmail.com>
2019-10-05 12:40:48 +1000

    sbt new outwatch/seed.g8

---
+++ frontend/src/test/scala/frontend/FrontendSpec.scala
@@ -0,0 +1,18 @@
+package frontend
+
+import org.scalatest._
+import org.scalajs.dom._
+import outwatch.dom._
+import outwatch.dom.dsl._
+import monix.execution.Scheduler.Implicits.global
+
+class FrontendSpec extends JSDomSpec {
+
+  "You" should "probably add some tests" in {
+
+    val message = "Hello World!"
+    OutWatch.renderInto("#app", h1(message)).unsafeRunSync()
+
+    document.body.innerHTML.contains(message) shouldBe true
+  }
+}
